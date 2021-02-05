0ac43981ff3ed87e01d745a83c65bde1b60b9ce6
Ricardo Silva <ricardani@gmail.com>
2021-02-05 19:17:19 +0000

    Add v1 of example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+/* eslint-disable react/no-unknown-property */
+import React from './react/react';
+import { render } from './react/react-dom';
+
+const App = () => (
+    <div className="main-class">
+        Hello World!
+        <span>1234</span>
+        <div>
+            <input type="text" name="txt" value="Hello" onChange={console.log} />
+        </div>
+    </div>
+);
+
+render(<App />, document.querySelector('#app'));
