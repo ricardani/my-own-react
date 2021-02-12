5f52cc6d5fee47008537493b6f479c7922f084a8
Ricardo Silva <ricardani@gmail.com>
2021-02-12 18:25:02 +0000

    Fix dependencies issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 /* eslint-disable react/no-unknown-property */
-import React from './react/react';
-import { render } from './react/react-dom';
+import React from './modules/react';
+import { render } from './modules/react-dom';
 
-const App = () => (
-    <div className="main-class">
-        Hello World!
-        <span>1234</span>
-        <div>
-            <input type="text" name="txt" value="Hello" onChange={console.log} />
+const useState = initialState => {
+    
+    let state = initialState;
+    const setState = newState => {
+        console.log('here')
+        state = newState;
+    }
+
+    return [state, setState];
+}
+
+const App = () => {
+    return (
+        <div className="main-class">
+            Hello a!
+            <span>1234</span>
+            <div>
+                <input type="text" name="txt" placeholder="Name" onchange={e => console.log('here', e.target.value)} />
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 render(<App />, document.querySelector('#app'));
